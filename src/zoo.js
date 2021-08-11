63b8034ce480600a84cf36515193935c7298603a
Elithon Silva <elithonsilva@gmail.com>
2021-08-11 07:53:22 -0300

    Requisito-04

--- src/zoo.js
+++ src/zoo.js
@@ -30,6 +30,10 @@ function getEmployeeByName(employeeName) {
 
 function createEmployee(personalInfo, associatedWith) {
   // seu código aqui
+  const createObject = { ...personalInfo, ...associatedWith};
+  employees.push(createObject);
+  return createObject;
+  // Utilizei o spread operator para espalhar os objetos recebidos em um novo objeto.
 }
 
 function isManager(id) {
